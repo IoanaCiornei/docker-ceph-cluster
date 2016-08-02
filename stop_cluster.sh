4f86c6a2d52a78eeec33a0a8d2701b6ccb081d8f
Ioana Ciornei <ciorneiioana@gmail.com>
2016-08-02 23:26:23 +0300

    log reworked on stop script
    Signed-off-by: Ioana Ciornei <ciorneiioana@gmail.com>

--- stop_cluster.sh
+++ stop_cluster.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+MODULE='STOP_CEPH_CLUSTER'
+
 source ./settings.sh
 
-# delete the containers
 for (( i = 0; i < $NUM_NODES; i++)); do
-	echo "Stopping Docker container ${node_name[$i]}..."
 	((j = i + 2))
 	NODE_IP="$IP_NETWORK$j"
-	sudo docker stop ${node_name[$i]}
+
+	log $MODULE "Stop Docker container ${node_name[$i]}..."
+	sudo docker stop ${node_name[$i]} &>> $DEBUG_FILE
 done
 
+log $MODULE "Stop ceph cluster... Finished!"
