e058dcb36b35b2198723d286d274445ec484f054
Ioana Ciornei <ciorneiioana@gmail.com>
2016-08-02 22:22:41 +0300

    changed comments into log messages
    Signed-off-by: Ioana Ciornei <ciorneiioana@gmail.com>

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,22 +1,25 @@
 #! /bin/bash
 
+MODULE='CREATE_CEPH_CLUSTER'
+
 source ./settings.sh
 
-set -x
-
+log $MODULE "Copy id_rsa to localhost..."
 ssh-copy-id localhost
 
-# create the cluster network
+log $MODULE "Create cluster network..."
 sudo docker network create --subnet=$NETWORK_IP.0/$NETWORK_MASK $CLUSTER_NETWORK
 
-# build the latest image of the ceph_node
+log $MODULE "Build the latest image of the ceph_node..."
 sudo docker build -t ceph_node .
 
-# temporary TODO
-sudo systemctl start ceph-mount-blk.service
+status=$(systemctl is-active ceph-mount-blk.service)
+if [[ "$status" == "inactive" ]]; then
+	log $MODULE "Start ceph-mount-blk service..."
+	sudo systemctl start ceph-mount-blk.service
+fi
 
-
-# array of loop devices
+log $MODULE "Retrieve block devices..."
 loop_devs=$(journalctl -b | grep "$LOGGER" | tail -n $NUM_DEVS | awk '{print $NF}')
 num=0
 for dev in $loop_devs; do
@@ -25,19 +28,19 @@ for dev in $loop_devs; do
 	((num++))
 done
 
-# create NUM_NODES containers
-num=0
-for (( i = 0; i < $NUM_NODES; i++)); do
+log $MODULE "Create containers..."
+for ((num=0, i = 0; i < $NUM_NODES; i++)); do
 	NODE_IP="$NETWORK_IP.$(($i+2))"
 
-	# start the container
+	log $MODULE "Create container ${node_name[$i]}..."
 	if (( i >= 1 && i <= $NUM_OSD )); then
 		# start the container with multiple disk if it is a OSD
 		FILE1=${devs[$num]} ; FILE1_p1="${devs[$num]}p1"; ((num++))
 		FILE2=${devs[$num]} ; FILE2_p1="${devs[$num]}p1"; ((num++))
 		FILE3=${devs[$num]} ; FILE3_p1="${devs[$num]}p1"; ((num++))
 		FILE4=${devs[$num]} ; FILE4_p1="${devs[$num]}p1"; FILE4_p2="${devs[$num]}p2"; FILE4_p3="${devs[$num]}p3"; ((num++))
-		
+
+		# create an OSD container
 		sudo docker run -d -it \
 			--privileged \
 			-v $FILE1:$FILE1 \
@@ -53,36 +56,37 @@ for (( i = 0; i < $NUM_NODES; i++)); do
 			-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 			--net ceph_network --ip $NODE_IP --hostname ${node_name[$i]} --name ${node_name[$i]} ceph_node
 	else
-		# admin, mon, client container
+		# create ADMIN or MON container
 		sudo docker run -d -it \
 			--privileged \
 			-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 			--net ceph_network --ip $NODE_IP --hostname ${node_name[$i]} --name ${node_name[$i]} ceph_node
 	fi
 
-	# add the new node into /etc/hosts
+	log $MODULE "Add the new node into /etc/hosts..."
 	echo -e "$NODE_IP\t${node_name[$i]}" | sudo tee -a /etc/hosts
 
-	# Wait for container to go up before issuing any more commands
+	log $MODULE "Wait for container to go up before issuing any more commands..."
 	while ! (sshpass -p $PASSWORD ssh $USER@${node_name[$i]} echo -e 'Host $HOSTNAME is up!'); do :; done
 
-	# copy my keys to the other hosts
-	sshpass -p $PASSWORD scp -v ~/.ssh/id_rsa ~/.ssh/id_rsa.pub $USER@${node_name[i]}:~/.ssh/
-	sshpass -p $ROOT_PASSWORD scp -v ~/.ssh/id_rsa ~/.ssh/id_rsa.pub root@${node_name[i]}:~/.ssh/
+	log $MODULE "Copy public keys og the host on the container..."
+	sshpass -p $PASSWORD scp -v ~/.ssh/id_rsa ~/.ssh/id_rsa.pub $USER@${node_name[$i]}:~/.ssh/
+	sshpass -p $ROOT_PASSWORD scp -v ~/.ssh/id_rsa ~/.ssh/id_rsa.pub root@${node_name[$i]}:~/.ssh/
 	sshpass -p $PASSWORD scp -v ~/.ssh/authorized_keys $USER@${node_name[$i]}:~/.ssh/authorized_keys
 	sshpass -p $ROOT_PASSWORD scp -v ~/.ssh/authorized_keys root@${node_name[$i]}:~/.ssh/authorized_keys
 
-	# give user sudo without password
+	log $MODULE "Give user $USER sudo rights without a password on container ${node_name[$i]}..."
 	CONFIG="$USER ALL=(ALL) NOPASSWD: ALL"
 	ssh -t root@${node_name[$i]} "echo \"$CONFIG\" >> /etc/sudoers"
 
+	log $MODULE "Disable strict host checking on container ${node_name[$i]}..."
 	ssh $USER@${node_name[$i]} "echo -e 'Host *\n\tStrictHostKeyChecking no' >> ~/.ssh/config"
 
 done
-
-
-# copy script to the admin node
-scp conf admin1.sh admin2.sh $USER@admin:.
-
-echo "Running nodes in cluster:"
+log $MODULE "Cluster nodes:"
 sudo docker ps
+log $MODULE "Create cluster nodes.... FINISHED"
+
+log $MODULE "Copy admin script on container..."
+scp conf admin1.sh $USER@admin:.
+
